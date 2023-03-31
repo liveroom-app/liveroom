e84cb9d1cef7ff57787a91f952e74adaa0a95b16
Basile Nouvellet <b.nouvellet@gmail.com>
2023-03-31 12:51:08 +0100

    Update cursor UI

--- lib/liveroom/colors.ex
+++ lib/liveroom/colors.ex
@@ -1,11 +1,55 @@
 defmodule Liveroom.Colors do
-  def get_hsl(string) do
-    hue =
-      string
-      |> to_charlist()
-      |> Enum.sum()
-      |> rem(360)
+  @colors [
+    # NOTE: List of Tailwind colors, shade 300.
+    #       See https://tailwindcss.com/docs/customizing-colors#default-color-palette
 
-    "hsl(#{hue}, 70%, 40%)"
+    # red-300
+    "#fca5a5",
+    # orange-300
+    "#fdba74",
+    # amber-300
+    "#fcd34d",
+    # yellow-300
+    "#fde047",
+    # lime-300
+    "#bef264",
+    # green-300
+    "#86efac",
+    # emerald-300
+    "#6ee7b7",
+    # teal-300
+    "#5eead4",
+    # cyan-300
+    "#67e8f9",
+    # sky-300
+    "#7dd3fc",
+    # blue-300
+    "#93c5fd",
+    # indigo-300
+    "#a5b4fc",
+    # violet-300
+    "#c4b5fd",
+    # purple-300
+    "#d8b4fe",
+    # fuchsia-300
+    "#f0abfc",
+    # pink-300
+    "#f9a8d4",
+    # rose-300
+    "#fda4af"
+  ]
+
+  def get_random_color do
+    Enum.random(@colors)
   end
+
+  # def get_hsl(string) do
+  #   hue =
+  #     string
+  #     |> to_charlist()
+  #     |> Enum.sum()
+  #     |> rem(360)
+
+  #   "hsl(#{hue}, 60%, 60%)"
+  # end
 end
--- lib/liveroom_web/components/playground.ex
+++ lib/liveroom_web/components/playground.ex
@@ -17,15 +17,15 @@ defmodule LiveroomWeb.Components.Playground do
         <ul id="playground_cursors" phx-hook="TrackCursorsHook" class="w-full h-full list-none p-8">
           <li
             :for={user <- @users}
-            :if={user.socket_id != @socket_id}
+            XXX:if={user.socket_id != @socket_id}
             style={"color: #{user.color}; left: calc(#{user.x}% - 11px); top: calc(#{user.y}% - 10px);"}
-            class="z-10 absolute flex flex-col justify-start items-start space-y-2 pointer-events-none"
+            class="z-10 absolute flex flex-col justify-start items-start space-y-1 pointer-events-none select-none"
           >
-            <.cursor class="shadow-2xl" />
+            <.cursor class="absolute top-0 left-0 shadow-2xl" />
 
             <span
               style={"background-color: #{user.color};"}
-              class="ml-4 py-1 px-3 text-sm text-white font-semibold select-none whitespace-nowrap overflow-hidden rounded-full shadow-2xl"
+              class="!mt-[26px] ml-[26px] py-1 px-3 text-sm text-brand font-semibold whitespace-nowrap rounded-full shadow-2xl"
             >
               <%= user.name %>
             </span>
@@ -33,7 +33,7 @@ defmodule LiveroomWeb.Components.Playground do
             <span
               :if={user.msg != ""}
               style={"border-color: #{user.color};"}
-              class="max-w-[20ch] ml-4 py-1 px-2 text-sm bg-white text-left select-none border rounded shadow-2xl"
+              class="max-w-[20ch] ml-[26px] py-1 px-2 text-sm bg-white text-left border rounded shadow-2xl"
             >
               <%= user.msg %>
             </span>
@@ -116,15 +116,25 @@ defmodule LiveroomWeb.Components.Playground do
     ~H"""
     <svg
       version="1.1"
-      class{@class}
-      width="25px"
-      height="25px"
+      class={["-rotate-[25deg]", @class]}
+      width="30px"
+      height="30px"
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
       viewBox="0 0 21 21"
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     >
-      <polygon fill="black" points="8.2,20.9 8.2,4.9 19.8,16.5 13,16.5 12.6,16.6" />
-      <polygon fill="currentColor" points="9.2,7.3 9.2,18.5 12.2,15.6 12.6,15.5 17.4,15.5" />
+      <filter id="shadow" color-interpolation-filters="sRGB">
+        <feDropShadow dx="1" dy="1" stdDeviation="0.5" flood-opacity="0.2" />
+      </filter>
+
+      <polygon points="8.2,20.9 8.2,4.9 19.8,16.5 13,16.5 12.6,16.6" />
+      <polygon points="9.2,7.3 9.2,18.5 12.2,15.6 12.6,15.5 17.4,15.5" />
+      <%!-- <polygon filter="url(#shadow)" points="8.2,20.9 8.2,4.9 19.8,16.5 13,16.5 12.6,16.6" />
+      <polygon filter="url(#shadow)" points="9.2,7.3 9.2,18.5 12.2,15.6 12.6,15.5 17.4,15.5" /> --%>
     </svg>
     """
   end
@@ -144,7 +154,7 @@ defmodule LiveroomWeb.Components.Playground do
           y: 50,
           msg: "",
           name: name,
-          color: Colors.get_hsl(name)
+          color: Colors.get_random_color()
         })
 
         LiveroomWeb.Endpoint.subscribe(@cursorview)
