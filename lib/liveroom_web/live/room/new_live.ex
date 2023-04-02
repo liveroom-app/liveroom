eb381026cf1707d75937d0a0aa543f842517204d
Basile Nouvellet <b.nouvellet@gmail.com>
2023-04-02 03:33:16 +0100

    All small caps

--- lib/liveroom_web/live/room/new_live.ex
+++ lib/liveroom_web/live/room/new_live.ex
@@ -9,7 +9,7 @@ defmodule LiveroomWeb.Room.NewLive do
     ~H"""
     <div class="min-h-screen w-full flex flex-col justify-center items-center">
       <div class="max-w-xs w-full flex flex-col items-center gap-14 rounded-lg bg-brand/5 border border-gray-400 shadow-lg p-8">
-        <h1 class="font-semibold text-brand text-lg">Create a New Room</h1>
+        <h1 class="font-semibold text-brand text-lg">create a new room</h1>
 
         <form
           id="room_form"
@@ -17,27 +17,30 @@ defmodule LiveroomWeb.Room.NewLive do
           phx-submit="save"
           class="w-full flex flex-col gap-4"
         >
+          <label for="title" class="font-semibold text-brand">name</label>
           <input
             class="rounded bg-brand/5 placeholder-gray-400/75 border-gray-400/20"
             type="text"
             name="title"
             label="Title"
-            placeholder="Title"
+            placeholder="dancing room"
           />
+
+          <label for="slug" class="font-semibold text-brand">slug</label>
           <input
             class="rounded bg-brand/5 placeholder-gray-400/75 border-gray-400/20"
             type="text"
             name="slug"
             label="Slug"
-            placeholder="Slug (5 letters is enough)"
+            placeholder="(5 letters is enough)"
           />
 
           <button
             type="submit"
             class="mt-4 bg-brand text-white font-semibold py-2 px-4 rounded shadow group"
-            phx-disable-with="Entering..."
+            phx-disable-with="entering..."
           >
-            Enter
+            enter
             <.icon
               name="hero-arrow-right-mini"
               class="h-4 w-4 ml-2 group-hover:translate-x-1 transition-transform"
