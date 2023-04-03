0df8a1e22b2e54715bee0d9441e8b68a39424ab6
Basile Nouvellet <b.nouvellet@gmail.com>
2023-04-03 15:18:59 +0100

    Hover on Join Waitlist button

--- lib/liveroom_web/live/home_live.ex
+++ lib/liveroom_web/live/home_live.ex
@@ -95,7 +95,7 @@ defmodule LiveroomWeb.HomeLive do
     <a
       href="https://tally.so/r/wQ1EvX"
       tabindex="-1"
-      class="rounded-[10px] bg-slate-900 text-white text-lg font-semibold px-6 py-4"
+      class="rounded-[10px] bg-slate-900 md:hover:bg-slate-900/90 text-white text-lg font-semibold px-6 py-4"
     >
       <%= render_slot(@inner_block) %>
     </a>
