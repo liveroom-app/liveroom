6a77ffe625cd7680d9803662bdb12514cd0f51ca
Basile Nouvellet <b.nouvellet@gmail.com>
2023-04-09 01:23:51 +0200

    Deactivate local memory swoosh causing problem in multi nodes setup

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,10 @@ import Config
 config :liveroom, LiveroomWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
-config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Liveroom.Finch
+config :swoosh,
+  local: false,
+  api_client: Swoosh.ApiClient.Finch,
+  finch_name: Liveroom.Finch
 
 # Do not print debug messages in production
 config :logger, level: :info
