34728519432ff8b6ad17f1b8559b96e246d58fe8
Basile Nouvellet <b.nouvellet@gmail.com>
2023-05-18 20:33:03 +0100

    Add screencast for fun

--- assets/js/hooks/joinCallHook.js
+++ assets/js/hooks/joinCallHook.js
@@ -5,13 +5,24 @@ export const JoinCallHook = {
   async initStream() {
     try {
       // Gets our local media from the browser and stores it as a const, stream.
+
+      // Webcam
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true,
         width: "1280",
       });
+
+      // Screencast
+      // const stream = await navigator.mediaDevices.getDisplayMedia({
+      //   audio: true,
+      //   video: true,
+      //   width: "1280",
+      // });
+
       // Stores our stream in the global constant, localStream.
       localStream = stream;
+
       // Sets our local video element to stream from the user's webcam (stream).
       document.getElementById("local-video").srcObject = stream;
     } catch (e) {
