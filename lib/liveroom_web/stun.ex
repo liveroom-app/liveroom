149fd65f2b728872f32110c8f7ad4213715be58c
Basile Nouvellet <b.nouvellet@gmail.com>
2023-05-18 20:32:07 +0100

    Bring back Stun

--- lib/liveroom_web/stun.ex
+++ lib/liveroom_web/stun.ex
@@ -1,14 +1,15 @@
-# defmodule LiveroomWeb.Stun do
-#   use GenServer
+defmodule LiveroomWeb.Stun do
+  use GenServer
 
-#   def start_link(_) do
-#     GenServer.start_link(__MODULE__, [])
-#   end
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [])
+  end
 
-# @impl true
-# def init(_) do
-#   :stun_listener.add_listener(3478, :udp, [])
+  @impl true
+  def init(_) do
+    {:ok, socket} = :gen_udp.open(0, [:binary, :inet])
+    {:ok, _} = :stun_listener.add_listener({127, 0, 0, 1}, 3478, :udp, [socket])
 
-#   {:ok, []}
-# end
-# end
+    {:ok, []}
+  end
+end
