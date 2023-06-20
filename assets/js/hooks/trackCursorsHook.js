edc777e7795f00d0ff9799b0daaeaf15fe96f65f
Basile Nouvellet <b.nouvellet@gmail.com>
2023-06-20 23:32:22 +0100

    More cleaning

--- assets/js/hooks/trackCursorsHook.js
+++ assets/js/hooks/trackCursorsHook.js
@@ -1,69 +1,40 @@
-// const MAX_FPS = 60;
-// const MAX_FPS = 30;
-// const MAX_FPS = 10;
-// const MILLISECONDS_PER_FRAME = 1000 / MAX_FPS;
-// const DEBOUNCE = (MILLISECONDS_PER_FRAME * 0.75).toFixed();
-// console.log("MILLISECONDS_PER_FRAME", MILLISECONDS_PER_FRAME);
-// console.log("DEBOUNCE", DEBOUNCE);
-
 const HALO_KEY = "Escape";
 const HALO_KEY_CODE = 27;
 
 export const TrackCursorsHook = {
   mounted() {
-    // void import("../../vendor/lodash_debounce.js").then(
-    //   ({ default: _debounce }) => {
-
     // Mouse move
     switch (this.el.dataset.mode) {
       case "container": {
-        this.el.addEventListener(
-          "mousemove",
-          // _debounce((e) => {
-          (e) => {
-            const xRatio = e.layerX / this.el.offsetWidth;
-            const x = Number(xRatio * 100).toFixed(2); // in %
+        this.el.addEventListener("mousemove", (e) => {
+          const xRatio = e.layerX / this.el.offsetWidth;
+          const x = Number(xRatio * 100).toFixed(2); // in %
 
-            const yRatio = e.layerY / this.el.offsetHeight;
-            const y = Number(yRatio * 100).toFixed(2); // in %
+          const yRatio = e.layerY / this.el.offsetHeight;
+          const y = Number(yRatio * 100).toFixed(2); // in %
 
-            this.pushEvent("liveroom-cursor-moved", { x, y });
-          }
-          // }, DEBOUNCE)
-        );
+          this.pushEvent("liveroom-cursor-moved", { x, y });
+        });
         break;
       }
       case "fullscreen": {
-        // document.addEventListener(
-        window.addEventListener(
-          "mousemove",
-          // _debounce((e) => {
-          (e) => {
-            const x = Number((e.pageX / window.innerWidth) * 100).toFixed(2); // in %
-            const y = Number((e.pageY / window.innerHeight) * 100).toFixed(2); // in %
+        window.addEventListener("mousemove", (e) => {
+          const x = Number((e.pageX / window.innerWidth) * 100).toFixed(2); // in %
+          const y = Number((e.pageY / window.innerHeight) * 100).toFixed(2); // in %
 
-            this.pushEvent("liveroom-cursor-moved", { x, y });
-          }
-          // }, DEBOUNCE)
-        );
+          this.pushEvent("liveroom-cursor-moved", { x, y });
+        });
         break;
       }
     }
 
     // Resize window
-    window.addEventListener(
-      "resize",
-      // _debounce((e) => {
-      (e) => {
-        this.pushEvent("liveroom-window-resize", {
-          inner_width: window.innerWidth,
-          inner_height: window.innerHeight,
-        });
-      }
-      // }, DEBOUNCE)
-    );
-    // }
-    // );
+    window.addEventListener("resize", (e) => {
+      this.pushEvent("liveroom-window-resize", {
+        inner_width: window.innerWidth,
+        inner_height: window.innerHeight,
+      });
+    });
 
     // Mouse click
     if (this.el.dataset.mouseclick) {
